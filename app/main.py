924961b2b1777f7cda732de98cb7f074e03f710c
arnold <adododjialban@gmail.com>
2025-11-03 18:36:07 +0000

    cors regler

--- app/main.py
+++ app/main.py
@@ -2,9 +2,25 @@ from fastapi import FastAPI, HTTPException
 from app.database import test_connection
 import asyncio
 from app.routes import user,template,cv,auth
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 
 app = FastAPI(title="Kauza'cv Backend")
+origins = [
+    "http://localhost:5173",   
+    # "https://ton-frontend-vercel.com",   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   
+    allow_credentials=True,
+    allow_methods=["*"],     
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 async def startup_event():
